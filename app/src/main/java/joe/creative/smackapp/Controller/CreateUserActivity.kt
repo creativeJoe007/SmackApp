aa0ad5029a6cbdacbb613e58ac385ebcbbf66610
Joseph Martin <creative.joe007@gmail.com>
2019-12-30 22:07:48 +0100

    Worked on auto generating background color and background image

--- app/src/main/java/joe/creative/smackapp/Controller/CreateUserActivity.kt
+++ app/src/main/java/joe/creative/smackapp/Controller/CreateUserActivity.kt
@@ -1,22 +1,48 @@
 package joe.creative.smackapp.Controller
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import joe.creative.smackapp.R
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
-
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
     fun generateUserAvatar(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        if(color === 0) {
+            userAvatar = "light$avatar"
+        } else {
+            userAvatar = "dark$avatar"
+        }
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceId)
     }
 
     fun generateColorClicked(view: View) {
+        val random = Random()
+        var r = random.nextInt(255)
+        var g = random.nextInt(255)
+        var b = random.nextInt(255)
+
+        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = r.toDouble() / 255
+        val savedB = r.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
 
     }
 
